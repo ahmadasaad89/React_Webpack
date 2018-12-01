ad005f1df5d6dbf98aecd4a08fef0fa078578e32
Ahmad Asaad <ahmadasaad89@users.noreply.github.com>
2018-12-01 12:58:30 +0100

    Adding SASS support

--- webpack.config.js
+++ webpack.config.js
@@ -49,8 +49,12 @@ module.exports =(env, argv) => {
                     use: ['babel-loader', 'eslint-loader']
                 },
                 {
-                    test: /\.css$/,
-                    use: ["style-loader", "css-loader"]
+                    test: /\.scss$/,
+                    use: [
+                        "style-loader", // creates style nodes from JS strings
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    ]
                 }
             ]
         },
