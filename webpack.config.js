031a8ac4a2da20fb4a8dd94185c4ea47d348c2c9
Ahmad Asaad <ahmadasaad89@users.noreply.github.com>
2018-12-03 13:21:14 +0100

    Moving index.html to the root folder

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports =(env, argv) => {
 
     const commonPlugins = [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "index.html"
         })
     ];
 
