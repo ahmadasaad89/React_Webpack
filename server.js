d4f66bb1530cf45376bd3b59d7519902d90cc1fe
Ahmad Asaad <ahmadasaad89@users.noreply.github.com>
2018-12-03 23:06:18 +0100

    Redirect all paths to index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 })
 
